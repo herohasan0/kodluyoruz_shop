1e0ce8dc340de108a0ef10f5aaa7b70cf9658de9
kahasan <kahraman.h@outlook.com>
2021-06-08 14:13:16 +0300

    Create basit router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import Card from './Card.js';
 import Navigation from './Navigation';
-
-import data from './api.json';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {data.map((item) => (
-        <Card
-          key={item.id}
-          id={item.id}
-          img={item.avatar}
-          title={item.title}
-          price={item.price}
-          alt={item.title}
-        />
-      ))}
     </div>
   );
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,33 +1,81 @@
+import React from 'react';
 import './Navigation.css';
+import Card from './Card.js';
+import data from './api.json';
 
-function Navigation() {
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+export default function Navigation() {
   return (
-    <nav className="Navigation">
-      <div className="left">
-        <img
-          className="logo"
-          alt="logo"
-          src="https://i.pinimg.com/originals/66/f7/72/66f77296282b5ab7c2780724802614c0.png"
-        />
-      </div>
-      <div className="right">
-        <ul className="links">
-          <li>
-            <a>Ana Sayfa</a>
-          </li>
-          <li>
-            <a>Favori</a>
-          </li>
-          <li>
-            <a>Sepetim</a>
-          </li>
-          <li>
-            <a>İletişim</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Router>
+      <nav className="Navigation">
+        <div className="left">
+          <img
+            className="logo"
+            alt="logo"
+            src="https://i.pinimg.com/originals/66/f7/72/66f77296282b5ab7c2780724802614c0.png"
+          />
+        </div>
+        <div className="right">
+          <ul className="links">
+            <li>
+              <Link to="/">Ana Sayfa</Link>
+            </li>
+            <li>
+              <Link to="/Favori">Favoriler</Link>
+            </li>
+            <li>
+              <Link to="/Cart">Sepetim</Link>
+            </li>
+            <li>
+              <Link to="/Iletisim">İletişim</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/Favori">
+          <Favori />
+        </Route>
+        <Route path="/Cart">
+          <Cart />
+        </Route>
+        <Route path="/Iletisim">
+          <Iletisim />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
-export default Navigation;
+function Cart() {
+  return <h2>Sepetim</h2>;
+}
+
+function Iletisim() {
+  return <h2>İletişim</h2>;
+}
+
+function Favori() {
+  return <h2>Favori</h2>;
+}
+function Home() {
+  return (
+    <>
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          id={item.id}
+          img={item.avatar}
+          title={item.title}
+          price={item.price}
+          alt={item.title}
+        />
+      ))}
+    </>
+  );
+}
